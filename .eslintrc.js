ab2b7d5777d797685292b86033d987695b4cd84b
johsam <j.samuelson64@gmail.com>
2020-05-09 20:32:10 +0200

    Updates

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,16 +9,17 @@ module.exports = {
         Ext: true,
     },
 
-    extends: ['eslint:recommended', 'plugin:flowtype/recommended', 'airbnb'],
+    extends: ['eslint:recommended', 'airbnb'],
     plugins: ['extjs', 'html', 'flowtype'],
     parserOptions: {
-        sourceType: 'module',
+        ecmaVersion: 2018,
     },
     rules: {
-        'indent': ['error', 4],
+        indent: ['error', 4],
         'linebreak-style': ['error', 'unix'],
-        'semi': ['error', 'always'],
-        'no-var': 0,
+        semi: ['error', 'always'],
+        'no-var': 1,
+        'prefer-const': 1,
         'vars-on-top': 0,
         'space-before-function-paren': 0,
         'object-shorthand': 0,
@@ -30,15 +31,19 @@ module.exports = {
         'no-underscore-dangle': 0,
         'no-shadow-restricted-names': 0,
         'max-len': ['error', 170],
-        'quotes': ['error', 'single', { avoidEscape: true }],
+        quotes: ['error', 'single', { avoidEscape: true }],
         'func-names': ['error', 'never'],
-        'comma-dangle': ['error', 'never'],
-        "no-unused-vars": [2, {"args": "all", "argsIgnorePattern": "^_"}],
+        'comma-dangle': 0,
+        'operator-linebreak': 0,
+        'no-unused-vars': [2, { args: 'all', argsIgnorePattern: '^_' }],
     },
 
     settings: {
         flowtype: {
             onlyFilesWithFlowAnnotation: true,
+        },
+        react: {
+            version: '999.999.999',
         },
     },
 };
